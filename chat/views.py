c3f043915a7d482886e3aea78e0b3cad5b734433
Herlina Pfeiffer <pfeiffer.herlina@gmx.net>
2023-04-25 11:06:02 +0200

    authentification

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .models import Message
+from .models import Chat, Message
 
 
 def index(request):
     if request.method == 'POST':
         print("Received data " + request.POST['textmassage'])
-
+        myChat = Chat.objects.get(id=1)
         Message.objects.create(
-            text=request.POST['textmassage'], chat=None, author=request.user, receiver=request.user)
-        return render(request, 'chat/index.html', {'username': 'Herlina'})
+            text=request.POST['textmassage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessage = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessage})

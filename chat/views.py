9d01a80e3c140b8eeacbc24f80787ece0be96659
Herlina Pfeiffer <pfeiffer.herlina@gmx.net>
2023-04-27 15:33:54 +0200

    login annotation

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Chat, Message
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url = '/login/')
 
 def index(request):
     if request.method == 'POST':
@@ -10,3 +14,14 @@ def index(request):
             text=request.POST['textmassage'], chat=myChat, author=request.user, receiver=request.user)
     chatMessage = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessage})
+
+def login_view(request):
+    redirect = request.GET.get('next')
+    if request.method == 'POST':
+        user = authenticate (username=request.POST['username'], password=request.POST['password'])
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get('redirect'))
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True}, {'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
